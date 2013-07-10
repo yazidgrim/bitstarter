a6ae1574cd8ae3a1a106af5c5afa2d938f666061
Yazid Grim <mohamed.yazid.grim@gmail.com>
2013-07-10 06:55:45 +0000

    pushed changes to web.js

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var BufFromFile = fs.readFileSync('./index.html');
+var text = BufFromFile.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- response.send('Hello World2!');
+ response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+//  console.log(textFromFile);   
 });
